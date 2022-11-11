5f8573137d70258833b09f56a11bd136226422e2
rubenconde1 <rubencnd96@gmail.com>
2022-11-11 12:39:59 +0100

    Código hasta login, register y logout

---
+++ blog-tutorial/src/Entity/Contact.php
@@ -0,0 +1,106 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ContactRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+use Symfony\Component\Validator\Constraints as Assert;
+
+#[ORM\Entity(repositoryClass: ContactRepository::class)]
+class Contact
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Field 'First Name' is mandatory")]
+    private $firstName;
+
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"Field 'Last Name' is mandatory")]
+    private $lastName;
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message: "Field 'Email' is mandatory")]
+    #[Assert\Email(message:"{{ value }} is not a valid email")]
+    private $email;
+
+
+    #[ORM\Column(length: 255)]
+    #[Assert\NotBlank(message:"Field 'Subject' is mandatory")]
+    private $subject;
+
+    #[ORM\Column(type: Types::TEXT)]
+    #[Assert\NotBlank(message:"Field 'Message' is mandatory")]
+    private ?string $message = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getFirstName(): ?string
+    {
+        return $this->firstName;
+    }
+
+    public function setFirstName(string $firstName): self
+    {
+        $this->firstName = $firstName;
+
+        return $this;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(string $email): self
+    {
+        $this->email = $email;
+
+        return $this;
+    }
+
+    public function getSubject(): ?string
+    {
+        return $this->subject;
+    }
+
+    public function setSubject(string $subject): self
+    {
+        $this->subject = $subject;
+
+        return $this;
+    }
+
+    public function getMessage(): ?string
+    {
+        return $this->message;
+    }
+
+    public function setMessage(string $message): self
+    {
+        $this->message = $message;
+
+        return $this;
+    }
+}
---
+++ blog-tutorial/src/Form/ContactFormType.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Contact;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+
+class ContactFormType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email')
+            ->add('subject')
+            ->add('message')
+            ->add('email', EmailType::class)
+            ->add('Send', SubmitType::class, array('label' => 'Send'));
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Contact::class,
+        ]);
+    }
+}
