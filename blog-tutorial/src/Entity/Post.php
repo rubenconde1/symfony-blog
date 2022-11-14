31a97943d0157143835c6b5f2a8c5ac8aa7b4f26
rubenconde1 <rubencnd96@gmail.com>
2022-11-14 13:48:36 +0100

    Realizado hasta el 5.2 Formulario

---
+++ blog-tutorial/src/Entity/Post.php
@@ -0,0 +1,146 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\PostRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: PostRepository::class)]
+class Post
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $title = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $content = null;
+
+    #[ORM\Column(type: Types::DATETIME_MUTABLE)]
+    private ?\DateTimeInterface $publishedAt = null;
+
+    #[ORM\ManyToOne(inversedBy: 'posts')]
+    private ?User $postUser = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $image = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $slug = null;
+
+    #[ORM\Column]
+    private ?int $numLikes = null;
+
+    #[ORM\Column]
+    private ?int $numComments = null;
+
+    public function __construct()
+    {
+        $this->publishedAt = new \DateTime();
+    }
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->title;
+    }
+
+    public function setTitle(string $title): self
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    public function getContent(): ?string
+    {
+        return $this->content;
+    }
+
+    public function setContent(string $content): self
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    public function getPublishedAt(): ?\DateTimeInterface
+    {
+        return $this->publishedAt;
+    }
+
+    public function setPublishedAt(\DateTimeInterface $publishedAt): self
+    {
+        $this->publishedAt = $publishedAt;
+
+        return $this;
+    }
+
+    public function getPostUser(): ?User
+    {
+        return $this->postUser;
+    }
+
+    public function setPostUser(?User $postUser): self
+    {
+        $this->postUser = $postUser;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    public function getSlug(): ?string
+    {
+        return $this->slug;
+    }
+
+    public function setSlug(string $slug): self
+    {
+        $this->slug = $slug;
+
+        return $this;
+    }
+
+    public function getNumLikes(): ?int
+    {
+        return $this->numLikes;
+    }
+
+    public function setNumLikes(int $numLikes): self
+    {
+        $this->numLikes = $numLikes;
+
+        return $this;
+    }
+
+    public function getNumComments(): ?int
+    {
+        return $this->numComments;
+    }
+
+    public function setNumComments(int $numComments): self
+    {
+        $this->numComments = $numComments;
+
+        return $this;
+    }
+}
