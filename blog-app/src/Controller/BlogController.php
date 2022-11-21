c1a18679c599f613df944b28583bb3f259b177d2
rubenconde1 <rubencnd96@gmail.com>
2022-11-21 13:47:08 +0100

    Creación de blog y page controller, y ajustes twig

---
+++ blog-app/src/Controller/BlogController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class BlogController extends AbstractController
+{
+    #[Route('/blog', name: 'app_blog')]
+    public function blog(): Response
+    {
+        return $this->render('blog/blog.html.twig', []);
+    }
+
+    #[Route('/single', name: 'single')]
+    public function single(): Response
+    {
+        return $this->render('blog/single.html.twig', []);
+    }
+}
